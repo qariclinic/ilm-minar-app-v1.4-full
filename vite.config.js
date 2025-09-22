8f821b79c2a6e6e725748d7a1f4292388b300f3b
qariclinic <hafizabunouman@gmail.com>
2025-09-22 21:36:14 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
+  build: {
+    outDir: 'dist'
+  }
 })
